ffdcbf001f79aeaf4a958d748d725553f96c8e41
Mohit Sharma <b25bs1205@iitj.ac.in>
2026-08-03 12:19:23 +0530

    ✅ Weather Observation Station 19 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-03

--- hackerrank/medium/weather-observation-station-19/solution.sql
+++ hackerrank/medium/weather-observation-station-19/solution.sql
@@ -1,5 +1,21 @@
-SELECT ROUND(((
-    MAX(LAT_N) - MIN(LAT_N)
-    +
-    MAX(LONG_W)- MIN(LONG_W))),4)
-    FROM STATION;
+SELECT ROUND(
+    SQRT(
+        POWER(
+            (
+                ABS(
+                MIN(LAT_N)-MAX(LAT_N)
+                )
+                ),
+                2)
+                +
+            POWER(
+                (
+                    ABS(
+                        MIN(LONG_W)-
+                        MAX(LONG_W))),2)
+                ),4)
+            
+            
+            
+            FROM STATION;
+
