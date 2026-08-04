0ca0789c38cc11a3b4e53951cafcb880d8ae62d4
Mohit Sharma <b25bs1205@iitj.ac.in>
2026-08-04 12:02:33 +0530

    ✅ The Report | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-04

--- hackerrank/medium/the-report/solution.sql
+++ hackerrank/medium/the-report/solution.sql
@@ -1,5 +1,16 @@
-select CO.CONTINENT, FLOOR(AVG(CT.POPULATION))
-FROM CITY CT
-JOIN COUNTRY CO
-ON CT.COUNTRYCODE=CO.CODE
-GROUP BY CO.CONTINENT;
+select 
+case 
+when g.Grade >=8 then s.Name
+else null end as Name,
+g.Grade,
+s.Marks
+from Students s
+join Grades g
+on s.Marks between g.Min_Mark and g.Max_Mark
+order by g.Grade desc,
+case when g.Grade >=  8 then s.Name end asc,
+case when g.Grade <8 then s.Marks end asc;
+
+
+
+
