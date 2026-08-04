cc8b4411a88e34bd4b2ce2321454d2320bda878f
Mohit Sharma <b25bs1205@iitj.ac.in>
2026-08-04 12:03:06 +0530

    ✅ The Report | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-04

---
+++ hackerrank/medium/full-score/solution.sql
@@ -0,0 +1,16 @@
+select 
+case 
+when g.Grade >=8 then s.Name
+else null end as Name,
+g.Grade,
+s.Marks
+from Students s
+join Grades g
+on s.Marks between g.Min_Mark and g.Max_Mark
+order by g.Grade desc,
+case when g.Grade >=  8 then s.Name end asc,
+case when g.Grade <8 then s.Marks end asc;
+
+
+
+
