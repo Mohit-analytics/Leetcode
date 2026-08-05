13d6295d74f62337480f2d874e78041fc5210ea1
Mohit Sharma <b25bs1205@iitj.ac.in>
2026-08-05 11:23:06 +0530

    ✅ Top Competitors | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-05

--- hackerrank/medium/full-score/solution.sql
+++ hackerrank/medium/full-score/solution.sql
@@ -1,16 +1,13 @@
-select 
-case 
-when g.Grade >=8 then s.Name
-else null end as Name,
-g.Grade,
-s.Marks
-from Students s
-join Grades g
-on s.Marks between g.Min_Mark and g.Max_Mark
-order by g.Grade desc,
-case when g.Grade >=  8 then s.Name end asc,
-case when g.Grade <8 then s.Marks end asc;
 
-
-
-
+SELECT s.hacker_id, h.name
+FROM Submissions s
+JOIN Challenges c
+    ON s.challenge_id = c.challenge_id
+JOIN Difficulty d
+    ON c.difficulty_level = d.difficulty_level
+JOIN Hackers h
+    ON s.hacker_id = h.hacker_id
+WHERE s.score = d.score
+GROUP BY s.hacker_id, h.name
+HAVING COUNT(*) > 1
+ORDER BY COUNT(*) DESC, s.hacker_id ASC;
